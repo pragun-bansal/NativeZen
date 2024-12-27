5879b64556bf42801ee694acc4640547aff25092
Shivam Gupta <gupta.shivam2604@gmail.com>
2024-12-27 15:10:14 +0530

    website build started

---
+++ nativezen/src/config/site.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+    name: "Native Zen",
+    url: "https://nativezen.design",
+    ogImage: "https://nativezen.design/og",
+    description:
+      "Beautifully designed landing page components built with React & Tailwind CSS.",
+    links: {
+      twitter: "https://twitter.com/nativezen",
+      discord: "https://discord.gg/87p2vpsat5",
+      github: "https://github.com/magicuidesign/nativezen",
+      instagram: "https://instagram.com/nativezen/",
+    },
+    keywords: [
+      "React",
+      "Tailwind CSS",
+      "Framer Motion",
+      "Landing Page",
+      "Components",
+      "Next.js",
+    ],
+  };
+  
+  export type SiteConfig = typeof siteConfig;
