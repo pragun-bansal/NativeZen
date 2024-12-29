0d0c85d12a8379b7a88edc73148a508a2495dbb0
Shivam Gupta <gupta.shivam2604@gmail.com>
2024-12-29 14:32:12 +0530

    threejs nextjs mc combo

--- nativezen/src/app/(marketing)/page.tsx
+++ nativezen/src/app/(marketing)/page.tsx
@@ -4,6 +4,7 @@
 // import Showcase from "@/components/sections/showcase";
 // import Testimonials from "@/components/sections/testimonials";
 
+import Model from "@/components/model";
 import Hero from "@/components/sections/hero";
 
 export default function Home() {
@@ -11,7 +12,8 @@ export default function Home() {
     <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
      
-      <Hero />
+      {/* <Hero /> */}
+      <Model />
       {/* <Showcase />
       <ComponentDemos />
       <Testimonials /> */}
--- nativezen/tailwind.config.cjs
+++ nativezen/tailwind.config.cjs
@@ -42,6 +42,13 @@ export default {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
